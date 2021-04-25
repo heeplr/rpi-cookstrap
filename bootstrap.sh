c9f4c39644bb1a49902dd098a102f557892745e0
daniel <daniel@localhost>
2021-04-25 11:46:17 +0200

    fix SC2162

--- bootstrap.sh
+++ bootstrap.sh
@@ -37,7 +37,7 @@ function error() {
 function help_for_vars() {
     vars="$1"
     for v in "${vars[@]}" ; do
-        IFS="|" read name description default <<< "${v}"
+        IFS="|" read -r name description default <<< "${v}"
         printf "%30s - %s (default: \"%s\")\n" "${name}" "${description}" "${default}"
     done
 }
