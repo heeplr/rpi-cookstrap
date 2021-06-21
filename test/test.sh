f98f22b363fae503eaee3d4955b13eaf70f359fe
daniel <daniel@localhost>
2021-06-21 21:19:22 +0200

    run shellcheck from test.sh

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# shellcheck
+shellcheck --check-sourced --external-sources --enable all --exclude SC2154 ./bootstrap-plugins/* ./bootstrap.sh || exit 1
+
+# bats tests
 for t in test/*.bats ; do
     ./test/bats/bin/bats "${t}" "$@" || exit 1
 done
